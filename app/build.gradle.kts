2bdc2df5b6f3fba7351d5fb8483d60024d8ec756
Fraggerz777 <146559854+Fraggerz777@users.noreply.github.com>
2025-03-17 16:37:59 +0700

    Added Login,Sign In Pages and Some Localization for different Height Phone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+   buildFeatures{
+       viewBinding = true
+   }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -39,7 +43,7 @@ android {
 dependencies {
 
     implementation(platform(libs.firebase.bom))
-
+    implementation (libs.google.material.v1110)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,6 +53,8 @@ dependencies {
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
