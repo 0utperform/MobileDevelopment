7d3a3b04a7879d4a4ae8485bc980cc4133c8a652
Fraggerz777 <146559854+Fraggerz777@users.noreply.github.com>
2025-05-14 15:17:54 +0700

    added current user information on edit profile(edit profile button functionality hasn't been implemented

--- app/src/main/java/com/example/a0utperform/ui/setting/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/example/a0utperform/ui/setting/editprofile/EditProfileActivity.kt
@@ -1,19 +1,75 @@
 package com.example.a0utperform.ui.setting.editprofile
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.a0utperform.R
 import com.example.a0utperform.databinding.ActivityEditProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
+import java.text.NumberFormat
+import java.util.Locale
 
+@AndroidEntryPoint
 class EditProfileActivity : AppCompatActivity() {
+    private val editProfileViewModel : EditProfileViewModel by viewModels()
     private lateinit var binding : ActivityEditProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        observeViewModels()
+
+    }
+
+    private fun observeViewModels() {
+        editProfileViewModel.isLoading.observe(this) { isLoading ->
+            showLoading(isLoading)
+        }
+        editProfileViewModel.userSession.observe(this) { session ->
+            session?.let {
+                editProfileViewModel.fetchPayroll(it.userId)
+                editProfileViewModel.fetchUserTeamAssignments(it.userId)
+                editProfileViewModel.fetchUserOutletAssignments(it.userId)
+                binding.etName.setText(it.name)
+                binding.etRole.setText(it.role)
+                binding.etAge.setText(it.age)
+
+
+            }
+        }
+        editProfileViewModel.payroll.observe(this) { payroll ->
+            val formatted = if (payroll != null) {
+                val currency = NumberFormat.getCurrencyInstance(Locale("in", "ID")).format(payroll)
+                getString(R.string.payroll_format, currency)
+            } else {
+                getString(R.string.payroll_format, "N/A")
+            }
+
+            binding.etPayroll.setText(formatted)
+        }
+
+        editProfileViewModel.teamDetail.observe(this) { teamList ->
+
+            if (teamList.isNullOrEmpty()) {
+                binding.etTeam.setText(getString(R.string.team_format, "N/A"))
+            } else {
+                val teamNames = teamList.joinToString { it?.name ?: "null" }
+                binding.etTeam.setText(getString(R.string.team_format, teamNames))
+            }
+        }
+
+        editProfileViewModel.outletDetail.observe(this) { outletList ->
+            val outletNames = outletList.joinToString { it.name ?: "N/A" }
+            binding.etOutlet.setText(getString(R.string.outlet_format, outletNames))
+        }
+
+    }
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
 }
---
+++ app/src/main/java/com/example/a0utperform/ui/setting/editprofile/EditProfileViewModel.kt
@@ -0,0 +1,82 @@
+package com.example.a0utperform.ui.setting.editprofile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.a0utperform.data.local.user.UserPreference
+import com.example.a0utperform.data.model.OutletDetail
+import com.example.a0utperform.data.model.TeamDetail
+import com.example.a0utperform.data.model.UserModel
+import com.example.a0utperform.data.repository.AuthRepository
+import com.example.a0utperform.data.repository.DatabaseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EditProfileViewModel @Inject constructor(
+    private val repository: AuthRepository,
+    private val databaseRepository: DatabaseRepository,
+    private val userPreference: UserPreference
+) : ViewModel() {
+
+    val userSession: LiveData<UserModel?> = userPreference.getSession().asLiveData()
+
+    private val _isLoading = MutableLiveData<Boolean>(false)
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _teamDetail = MutableLiveData<List<TeamDetail?>>()
+    val teamDetail: LiveData<List<TeamDetail?>> = _teamDetail
+
+    private val _outletDetail = MutableLiveData<List<OutletDetail>>()
+    val outletDetail: LiveData<List<OutletDetail>> = _outletDetail
+
+    private val _avatarUrl = MutableLiveData<String?>()
+    val avatarUrl: LiveData<String?> = _avatarUrl
+
+    private val _payroll = MutableLiveData<Double?>()
+    val payroll: LiveData<Double?> = _payroll
+
+    fun fetchPayroll(userId: String) {
+        viewModelScope.launch {
+            _isLoading.value = true
+            val result = databaseRepository.getUserPayroll(userId)
+            if (result.isSuccess) {
+                _payroll.value = result.getOrNull()
+            } else {
+                _payroll.value = null
+            }
+            _isLoading.value = false
+        }
+    }
+
+    // Fetch User Team Assignments
+    fun fetchUserTeamAssignments(userId: String) {
+        viewModelScope.launch {
+            _isLoading.value = true
+            val teamResult = databaseRepository.getAssignedTeamDetails(userId)
+            if (teamResult.isSuccess) {
+                _teamDetail.value = teamResult.getOrNull() ?: emptyList()
+            } else {
+                _teamDetail.value = emptyList()
+            }
+            _isLoading.value = false
+        }
+    }
+
+    // Fetch User Outlet Assignments
+    fun fetchUserOutletAssignments(userId: String) {
+        viewModelScope.launch {
+            _isLoading.value = true
+            val outletResult = databaseRepository.getAssignedOutletDetails(userId)
+            if (outletResult.isSuccess) {
+                _outletDetail.value = outletResult.getOrNull() ?: emptyList()
+            } else {
+                _outletDetail.value = emptyList()
+            }
+            _isLoading.value = false
+        }
+    }
+}
